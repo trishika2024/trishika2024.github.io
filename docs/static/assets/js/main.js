64cb4b6fa55022161376bd1eed59bd6238b605e6
trishika2024 <trishikasolutionspvtltd@gmail.com>
2025-01-20 12:08:36 +0000

    Website Pushed

--- docs/static/assets/js/main.js
+++ docs/static/assets/js/main.js
@@ -90,19 +90,6 @@
   document.addEventListener('scroll', toggleScrollTop);
 
   /**
-   * Animation on scroll function and init
-   */
-  function aosInit() {
-    AOS.init({
-      duration: 600,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    });
-  }
-  window.addEventListener('load', aosInit);
-
-  /**
    * Initiate glightbox
    */
   const glightbox = GLightbox({
@@ -166,19 +153,6 @@
         filter: filter,
         sortBy: sort
       });
-    });
-
-    isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
-      filters.addEventListener('click', function() {
-        isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
-        this.classList.add('filter-active');
-        initIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        if (typeof aosInit === 'function') {
-          aosInit();
-        }
-      }, false);
     });
 
   });
