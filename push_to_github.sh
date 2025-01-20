5c00f88cef3ab307eeced7ba76ad735d7804455c
trishika2024 <trishikasolutionspvtltd@gmail.com>
2025-01-20 12:20:31 +0000

    Website Pushed

--- push_to_github.sh
+++ push_to_github.sh
@@ -2,11 +2,13 @@
 /opt/trishikasolutions/env/bin/python /opt/trishikasolutions/manage.py distill-local /home/badal/trishika2024.github.io/docs/
 cd trishika2024.github.io
 
+cd docs
 cat <<EOF >CNAME
 trishikasolutions.com
 EOF
 
 cp -r /opt/trishikasolutions/media .
+cd ..
 git add .
 git commit -m "Website Pushed"
 git push origin
