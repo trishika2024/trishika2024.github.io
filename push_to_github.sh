a55b2868a3b73a2f4604d9e1b15b12b22e24746f
trishika2024 <trishikasolutionspvtltd@gmail.com>
2025-02-11 04:17:28 +0000

    Website Pushed

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 /opt/trishikasolutions/env/bin/python /opt/trishikasolutions/manage.py distill-local /home/badal/trishika2024.github.io/docs/
-cd trishika2024.github.io
+cd /home/badal/trishika2024.github.io
 
-cd docs
+cd /home/badal/trishika2024.github.io/docs
 cat <<EOF >CNAME
 www.trishikasolutions.com
 EOF
 
-cp -r /opt/trishikasolutions/media .
+cp -r /opt/trishikasolutions/media /home/badal/trishika2024.github.io/docs/
 cd /home/badal/trishika2024.github.io
 
 git add .
